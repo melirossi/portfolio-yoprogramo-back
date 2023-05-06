c3d316ad2220d3769e371e54479202d9e7e4fb87
Melisa <melisa.s.rossi@gmail.com>
2023-05-06 12:09:39 -0300

    fix cors

--- src/main/java/com/portfolio/melisa/Controller/CHabilidadesBlandas.java
+++ src/main/java/com/portfolio/melisa/Controller/CHabilidadesBlandas.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/habblandas")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://melisa-portfolio.web.app","http://localhost:4200"})
 public class CHabilidadesBlandas {
     @Autowired
     SHabilidadesBlandas sHabilidadesBlandas;
--- src/main/java/com/portfolio/melisa/Controller/CHabilidadesDuras.java
+++ src/main/java/com/portfolio/melisa/Controller/CHabilidadesDuras.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/habduras")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://melisa-portfolio.web.app","http://localhost:4200"})
 public class CHabilidadesDuras {
     
     @Autowired
