c407f739af8a7cd1440a5117fe4757665a0e3c14
Melisa <melisa.s.rossi@gmail.com>
2023-05-11 10:58:10 -0300

    clean code security

--- src/main/java/com/portfolio/melisa/Security/Dto/LoginUsuario.java
+++ src/main/java/com/portfolio/melisa/Security/Dto/LoginUsuario.java
@@ -1,24 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.portfolio.melisa.Security.Dto;
 
 import javax.validation.constraints.NotBlank;
 
-/**
- *
- * @author Usuario
- */
 public class LoginUsuario {
     @NotBlank
     private String nombreUsuario;
     @NotBlank
     private String password;
     
-    //Getter & Setter
-
     public String getNombreUsuario() {
         return nombreUsuario;
     }
--- src/main/java/com/portfolio/melisa/Security/Entity/UsuarioPrincipal.java
+++ src/main/java/com/portfolio/melisa/Security/Entity/UsuarioPrincipal.java
@@ -15,7 +15,6 @@ public class UsuarioPrincipal implements UserDetails {
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
 
-    //Constructor
     public UsuarioPrincipal(String nombre, String nombreUsuario, String email, String password, Collection<? extends GrantedAuthority> authorities) {
         this.nombre = nombre;
         this.nombreUsuario = nombreUsuario;
